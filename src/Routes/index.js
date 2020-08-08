5773939fe78539ee31d8210e3a08739bfe022616
Johan Espino <johanespino9@gmail.com>
2020-08-08 17:52:42 -0500

    [ADD] Privatizando rutas

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,16 +1,27 @@
 import React from 'react'
-import { Route, Switch} from 'react-router-dom'
+import { Route, Switch, Redirect} from 'react-router-dom'
 import Inicio from '../Contenedores/Inicio'
 import Productos from '../Contenedores/Productos'
 import Login from '../Contenedores/Login'
 import RegistroUsuario from '../Contenedores/RegistroUsuario'
+
+const PrivateRoutes = ({ path, component }) => {
+
+    const usuarioLogueado = localStorage.getItem("user")
+
+    if(usuarioLogueado){
+        return <Route path={path} component={component}/>
+    }
+
+    return <Redirect to="/login"/>
+}
 
 const Routes = () => {
     
     return(
         <Switch>
             <Route exact path="/" component={Inicio}/>
-            <Route path="/productos" component={Productos}/>
+            <PrivateRoutes path="/productos" component={Productos}/>
             <Route path="/login" component={Login}/>
             <Route paht="/registro" component={RegistroUsuario}/>
         </Switch>
