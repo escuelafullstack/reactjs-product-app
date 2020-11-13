15314d4a79f96f5bb1dcd257bb04636fad8cb00e
Johan Espino <johanespino9@gmail.com>
2020-11-13 01:43:01 -0500

    [UPDATE] build app2

---
+++ build/precache-manifest.e5c877bdc9460b466e1294018538c37c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ac72172c988987f21c6e1d427785c51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aeec7ef774f8dc5322e5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d7df49d9a5e58410cbc1",
+    "url": "/static/js/2.25eaec4b.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.25eaec4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeec7ef774f8dc5322e5",
+    "url": "/static/js/main.d453fe66.chunk.js"
+  },
+  {
+    "revision": "158a4d71f9f21104a97b",
+    "url": "/static/js/runtime-main.a0783a36.js"
+  }
+]);
